3b3cd5d81070357e1a4b44561272d69aaeaaa039
ph piment <ph.piment@gmail.com>
2020-03-01 03:01:26 +0900

    [add] set .env

--- docker/composeexample/settings.py
+++ docker/composeexample/settings.py
@@ -11,10 +11,15 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+import environ
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+env = environ.Env()
+root = environ.Path(__file__) - 2
+env_file = str(root.path('.env'))
+env.read_env(env_file)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
@@ -80,7 +85,7 @@ DATABASES = {
         'USER': 'postgres',
         'HOST': 'db',
         'PORT': 5432,
-        'PASSWORD': 'password',
+        'PASSWORD': env('POSTGRES_PASSWORD'),
     }
 }
 
